e59814d056de855df03d4ee212b5106edc71ad07
HumpyMiryala <hampinetha@msitprogram.net>
2021-07-20 16:08:12 +0530

    done

--- 10-numberofpoolballs-Python/numberofpoolballs.py
+++ 10-numberofpoolballs-Python/numberofpoolballs.py
@@ -7,6 +7,13 @@
 # limit our analysis to a "rack" of 15 balls. Rather, our pool table can contain an unlimited 
 # number of rows. For this problem and the next, you should research Triangular Numbers.
 
-def numberofpoolballs(rows):
-	# Your code goes here
-	pass
+def numberofpoolballs(balls):
+	
+	l=list()
+	for i in range(rows):
+		if i<=rows:
+	for i in range(balls+1):
+		if i<=balls:
+			l.append(i)
+			s=sum(l)
+	return s
