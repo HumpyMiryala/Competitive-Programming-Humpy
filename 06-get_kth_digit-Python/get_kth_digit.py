47b7403fbafbf69c377bdf63332c5171639e0b30
HumpyMiryala <hampinetha@msitprogram.net>
2021-07-20 15:38:18 +0530

    done

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -3,7 +3,6 @@
 # and returns the kth digit of n, starting from 0, counting from the right.
 # if the kth digit is not present return 0 
 
-
-
 def fun_get_kth_digit(digit, k):
-	return 0
+    	
+	return (abs(digit)//(10**(k))%10)
