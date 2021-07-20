e80f5a71d29432298f31cb57396be9340c86135c
HumpyMiryala <hampinetha@msitprogram.net>
2021-07-20 15:36:56 +0530

    done

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -4,4 +4,17 @@
 
 def hasconsecutivedigits(n):
 	# your code goes here
-	pass
+	n = abs(n)
+	flag = False
+	previousDigit = -1
+	while n > 0:
+		lastDigit = n % 10
+		n = n // 10
+		if lastDigit == previousDigit - 1:
+			flag = True
+			print(lastDigit,previousDigit)
+		# return True
+		# if lastDigit != previousDigit:
+		#     print(lastDigit, previousDigit)
+		previousDigit = lastDigit
+	return flag
