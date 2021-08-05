8441b2ab840df196830c8de46bdd4e8d711c73d1
HumpyMiryala <hampinetha@msitprogram.net>
2021-08-05 07:37:28 +0530

    done

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -6,6 +6,19 @@
 # For example, 313 is a palindrome and it is prime 
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) 
 # returns 2
+def ispalindromeprime(n):
 
-def fun_nth_palindromic_prime(n):
-	return 0
+	reverse=int(str(n)[::-1])
+	if n==reverse:
+		count=0
+		# if x>1:
+		for i in range(1,n+1):
+			if(n%i)==0:
+				count = count+1
+		if count == 2:
+			return True
+		else:
+			return False
+	else:
+		return False
+print(ispalindromeprime(n))
